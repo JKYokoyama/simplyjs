2e11f6de4cbfef667d69ee2f0c3b48e858470916
JKYokoyama <JKYokoyama@gmail.com>
2013-12-15 15:57:00 -0800

    We'll work on this

--- Var.js
+++ Var.js
@@ -1,3 +1,30 @@
+console.log('Simply.js demo!');
+
+simply.on('singleClick', function(e) {
+  console.log(util2.format('single clicked $button!', e));
+  simply.setText({
+    subtitle: 'Pressed ' + e.button + '!',
+  });
+});
+
+simply.on('longClick', function(e) {
+  console.log(util2.format('long clicked $button!', e));
+});
+
+simply.on('accelTap', function(e) {
+  console.log(util2.format('tapped accel axis $axis $direction!', e));
+  simply.setText({
+    subtitle: 'Tapped ' + (e.direction > 0 ? '+' : '-') + e.axis + '!',
+  });
+});
+
+simply.setText({
+  title: 'Let\'s have some fun!',
+  body: 'Bear with me ʕ•ᴥ•ʔ'
+}, true);
+
+simply.begin();
+
 var change_text='punch';
 
 var fightList=[
