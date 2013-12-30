851b0df22a315979b1ec4a3ff89ea752af41614a
JKYokoyama <JKYokoyama@gmail.com>
2013-12-29 23:23:00 -0800

    Update Rwotd.js

--- Rwotd.js
+++ Rwotd.js
@@ -2,5 +2,5 @@
 var wordUrl = 'http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5';
 
 ajax({ url: wordUrl, type: 'json' }, function(data) {
-    simply.text({ title: data.word }, true);
+    simply.text({ title: data[0].word }, true);
 });
