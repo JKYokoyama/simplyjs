f5949c28f34a6fdc9d634b6337444229a0b7cd53
JKYokoyama <JKYokoyama@gmail.com>
2013-12-30 00:52:35 -0800

    Update res-teamselect.js

--- res-teamselect.js
+++ res-teamselect.js
@@ -1,21 +1,29 @@
-UI.initTeamSelect = function() {
-  UI.showGameState = function()  {
-    simply.setText({title: 'Team Select'});
-  }
+var UI = {};
+
+UI.Flow = {};
+
+var TeamSelect = {};
+
+TeamSelect.update = function() {
   var list = [];
-    for (var i == 0 ; i < players.length ; ++i){    
-      var player = players[i];
-      var marker = player.isLeader? 'L': ' ';
-      if(i==showGameState.cursorIndex){
-        marker = 'o';
-      }
-      if(i==showGameState.selectedIndex){
-        marker = 'x';
-      }
-      list.push('[' + marker + ']' + ' ' + player.name)
+  for (var i = 0 ; i < players.length ; ++i) {    
+    var player = players[i];
+    var marker = player.isLeader? 'L': ' ';
+    if (i == showGameState.cursorIndex) {
+      marker = 'o';
     }
+    if (i == showGameState.selectedIndex) {
+      marker = 'x';
+    }
+    list.push('[' + marker + ']' + ' ' + player.name);
+  }
   list.push('done');
   simply.setText({
     body: list.join('\n')
   });
+};
+
+TeamSelect.show = function() {
+  simply.setText({title: 'Team Select'});
+  TeamSelect.update();
 };
